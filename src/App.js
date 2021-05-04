19d81241c6e58f195fc84aab4a93de234e0b1eeb
Paddy McGlone <paddymcglone@gmail.com>
2021-05-04 20:49:03 +0100

    Adding last bit of code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import { Card } from './components/styles';
+import React from 'react';
+import './App.scss';
+import { Card } from './components/Card';
+import movies from './data';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
-    </div>
+    <main>
+      {movies.map( movie => (
+        <Card classes="mr" key={`${movie.id}`}> 
+          <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </main>
   );
 }
 
